e5488ea787a4e76a26eae935df8b93997914753c
TCK1997 <achinkhang97.sg@gmail.com>
2019-09-19 19:16:36 +0800

    Edit SnoozeTest.java

--- src/test/java/SnoozeTest.java
+++ src/test/java/SnoozeTest.java
@@ -12,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class SnoozeTest {
+
     @Test
     public void snoozeCommand_rescheduleDeadline() throws DukeException {
         TaskList tasks = new TaskList();
@@ -20,12 +21,13 @@ public class SnoozeTest {
 
         Command addInitialTask = new Parser().parse("deadline TestDeadline /by 09/19/2019 12:00");
         addInitialTask.execute(tasks, storage, ui);
-        assertEquals( "09/19/2019 12:00", tasks.getTasks().get(0).getStartDate().toString());
+        assertEquals("09/19/2019 12:00", tasks.getTasks().get(0).getStartDate().toString());
         Command rescheduleTask = new Parser().parse("snooze 1 09/20/2019 13:11");
         rescheduleTask.execute(tasks, storage, ui);
-        assertEquals( "09/20/2019 13:11", tasks.getTasks().get(0).getStartDate().toString());
+        assertEquals("09/20/2019 13:11", tasks.getTasks().get(0).getStartDate().toString());
     }
 
+    @Test
     public void snoozeCommand_rescheduleEvent() throws DukeException {
         TaskList tasks = new TaskList();
         Ui ui = new Ui();
@@ -36,14 +38,14 @@ public class SnoozeTest {
                 .parse("event TestEvent /at 09/19/2019 12:00 to 10/20/2019 00:01");
         addInitialTask.execute(tasks, storage, ui);
         testEvent = (Event) tasks.getTasks().get(0);
-        assertEquals( "E | 0 | TestEvent | 09/19/2019 12:00 | 10/20/2019 00:01",
+        assertEquals("E | 0 | TestEvent | 09/19/2019 12:00 | 10/20/2019 00:01",
                 testEvent.storeString());
 
 
         Command rescheduleTask = new Parser()
                 .parse("snooze 1 09/20/2019 13:11 to 10/20/2019 00:01");
         rescheduleTask.execute(tasks, storage, ui);
-        assertEquals( "E | 0 | TestEvent | 09/20/2019 13:11 | 10/20/2019 00:01",
+        assertEquals("E | 0 | TestEvent | 09/20/2019 13:11 | 10/20/2019 00:01",
                 testEvent.storeString());
     }
 }
