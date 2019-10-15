f401203c702414165dabd19fc77c958363653fb6
ttippinyu <StartNow25>
2019-10-15 14:21:00 +0800

    deconflict

--- src/test/java/ReminderCommandTest.java
+++ src/test/java/ReminderCommandTest.java
@@ -17,11 +17,7 @@ public class ReminderCommandTest {
         TaskList testTaskList = new TaskList();
         long overdueDate = 656740800000L;
 
-<<<<<<< HEAD
-        testTaskList.add(new Deadline("overdue", new Date(overdueDate)));
-=======
         testTaskList.add(new Deadline("overdue", new DateTime(new Date(overdueDate))));
->>>>>>> 3d141ea8e35037ac3c80a5acbb40d1ef3e369ebd
 
         Ui testUi = new Ui();
         Storage testStorage = new Storage("data/dukeTest.txt");
@@ -34,11 +30,7 @@ public class ReminderCommandTest {
         TaskList testTaskList = new TaskList();
         long overdueDate = 656740800000L;
 
-<<<<<<< HEAD
-        testTaskList.add(new Deadline("overdue", new Date(overdueDate)));
-=======
         testTaskList.add(new Deadline("overdue", new DateTime(new Date(overdueDate))));
->>>>>>> 3d141ea8e35037ac3c80a5acbb40d1ef3e369ebd
         testTaskList.mark(0);
 
         Ui testUi = new Ui();
@@ -53,11 +45,7 @@ public class ReminderCommandTest {
         TaskList testTaskList = new TaskList();
         long distantFutureDate = 1893427200000L;
 
-<<<<<<< HEAD
-        testTaskList.add(new Deadline("future", new Date(distantFutureDate)));
-=======
         testTaskList.add(new Deadline("future", new DateTime(new Date(distantFutureDate))));
->>>>>>> 3d141ea8e35037ac3c80a5acbb40d1ef3e369ebd
 
         Ui testUi = new Ui();
         Storage testStorage = new Storage("data/dukeTest.txt");
