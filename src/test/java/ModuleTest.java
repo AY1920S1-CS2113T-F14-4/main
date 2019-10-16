68fc5ec8f6d11f9c9d6e9e8a4cffc7d1890e9d03
rssujay <rssujay97@gmail.com>
2019-10-17 00:34:45 +0800

    Fix unit test potential conflicts

--- src/test/java/ModuleTest.java
+++ src/test/java/ModuleTest.java
@@ -17,43 +17,43 @@ public class ModuleTest {
     public void moduleCreation_variousModules_successfulCreationWithWorkingGetters() throws FileCreationException,
             CorruptedDataException, DataReadWriteException {
         ModuleContainer testContainer = new ModuleContainer();
-        Module testModuleOne = new Module("testModCode1", "Engineering Principles & Practice I");
+        Module testModuleOne = new Module("testMod1", "Engineering Principles & Practice I");
 
-        assertEquals(testModuleOne.getModuleCode(), "testModCode1");
+        assertEquals(testModuleOne.getModuleCode(), "testMod1");
         assertEquals(testModuleOne.getModuleName(), "Engineering Principles & Practice I");
-        assertEquals(testModuleOne.storeString(), "testModCode1 | Engineering Principles & Practice I");
+        assertEquals(testModuleOne.storeString(), "testMod1 | Engineering Principles & Practice I");
 
-        Module testModuleTwo = new Module("testMod Code2", "Discrete Structures");
+        Module testModuleTwo = new Module("testMod2", "Discrete Structures");
 
-        assertEquals(testModuleTwo.getModuleCode(), "testMod Code2");
+        assertEquals(testModuleTwo.getModuleCode(), "testMod2");
         assertEquals(testModuleTwo.getModuleName(), "Discrete Structures");
-        assertEquals(testModuleTwo.storeString(), "testMod Code2 | Discrete Structures");
+        assertEquals(testModuleTwo.storeString(), "testMod2 | Discrete Structures");
 
-        Module testModuleThree = new Module("testModCode3", "Engineering Principles & Practice II");
+        Module testModuleThree = new Module("testMod3", "Engineering Principles & Practice II");
 
-        assertEquals(testModuleThree.getModuleCode(), "testModCode3");
+        assertEquals(testModuleThree.getModuleCode(), "testMod3");
         assertEquals(testModuleThree.getModuleName(), "Engineering Principles & Practice II");
-        assertEquals(testModuleThree.storeString(), "testModCode3 | Engineering Principles & Practice II");
+        assertEquals(testModuleThree.storeString(), "testMod3 | Engineering Principles & Practice II");
     }
 
     @Test
     public void storageStringRecreation_oneModule_expectedRecreatedObject() throws StorageException {
         ModuleContainer testContainer = new ModuleContainer();
-        Module testModuleOne = new Module("testModCode1", "Engineering Principles & Practice I");
+        Module testModuleOne = new Module("testMod4", "Engineering Principles & Practice I");
 
-        assertEquals(testModuleOne.getModuleCode(), "testModCode1");
+        assertEquals(testModuleOne.getModuleCode(), "testMod4");
         assertEquals(testModuleOne.getModuleName(), "Engineering Principles & Practice I");
 
         Module testModuleOneRecreated = new Module(testModuleOne.storeString());
-        assertEquals(testModuleOneRecreated.getModuleCode(), "testModCode1");
+        assertEquals(testModuleOneRecreated.getModuleCode(), "testMod4");
         assertEquals(testModuleOneRecreated.getModuleName(), "Engineering Principles & Practice I");
-        assertEquals(testModuleOneRecreated.storeString(), "testModCode1 | Engineering Principles & Practice I");
+        assertEquals(testModuleOneRecreated.storeString(), "testMod4 | Engineering Principles & Practice I");
     }
 
     @Test
     public void loadDataFromStorageSuccessful_oneModule_expectedFilesLoaded() throws StorageException {
         ModuleContainer testContainer = new ModuleContainer();
-        Module testModuleOne = new Module("testModCode1", "Engineering Principles & Practice III");
+        Module testModuleOne = new Module("testMod5", "Engineering Principles & Practice III");
 
         testModuleOne.getFiles().add(new File(0, "testFile1"));
         testModuleOne.getGrades().add(new GradedComponent("Essay", 20));
