c9e3b1e0d58e3f66b33f4ffa8f1e10e34ba8e834
TCK1997 <achinkhang97.sg@gmail.com>
2019-10-17 22:29:50 +0800

    Modify JUnit Method Names

--- src/test/java/DateTimeTest.java
+++ src/test/java/DateTimeTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DateTimeTest {
 
     @Test
-    public void correctDateInputShouldCreateReusableString() throws SpinBoxException {
+    public void dateTimeCreation_variousDateTimeString_successfulCreationAndExpectedStringOutput() {
         DateTime test = new DateTime("05/02/2020 12:00");
         assertEquals("05/02/2020 12:00", test.toString());
 
@@ -25,7 +25,7 @@ public class DateTimeTest {
     }
 
     @Test
-    public void parseDifferentDateFormattedString() throws SpinBoxException {
+    public void dateTimeCreation_differentlyFormattedDateTimeString_successfulCreationAndExpectedStringOutput() {
         DateTime test = new DateTime("the day before 12/16/2019 4pm");
         assertEquals("12/15/2019 16:00", test.toString());
 
@@ -42,47 +42,47 @@ public class DateTimeTest {
     }
 
     @Test
-    public void getDayOfMonthOfDateTimeStringShouldReturnCorrectDayInteger() {
+    public void getDayOfMonth_DateTimeString_expectedDay() {
         DateTime test = new DateTime("10/16/2019 12:33");
         assertEquals(16, test.getDayOfMonth());
     }
 
     @Test
-    public void getDayOfWeekOfDateTimeStringShouldReturnCorrectDayInteger() {
+    public void getDayOfWeek_DateTimeString_expectedDay() {
         DateTime test = new DateTime("10/16/2019 12:33");
         assertEquals(4, test.getDayOfWeek());
     }
 
     @Test
-    public void getHourFromDateTimeStringShouldReturnCorrectHourInteger() {
+    public void getHour_DateTimeString_expectedHour() {
         String date = "10/16/2019 12:33";
         DateTime test = new DateTime(date);
         assertEquals(12, test.getHour());
     }
 
     @Test
-    public void getStartOfTheWeekFromDateTimeStringShouldReturnCorrectDateTimeString() {
+    public void getStartOfTheWeek_DateTimeString_expectedDateTimeString() {
         String date = "10/16/2019";
         DateTime test = new DateTime(date + " 00:00");
         assertEquals("10/13/2019 00:00", test.getStartOfTheWeek().toString());
     }
 
     @Test
-    public void getEndOfTheWeekFromDateTimeStringShouldReturnCorrectDateTimeString() {
+    public void getEndOfTheWeek_DateTimeString_expectedDateTimeString() {
         String date = "10/16/2019";
         DateTime test = new DateTime(date + " 23:59");
         assertEquals("10/19/2019 23:59", test.getEndOfTheWeek().toString());
     }
 
     @Test
-    public void getStartOfTheMonthFromDateTimeStringShouldReturnCorrectDateTimeString() {
+    public void getStartOfTheMonth_DateTimeString_expectedDateTimeString() {
         String date = "10/16/2019";
         DateTime test = new DateTime(date + " 00:00");
         assertEquals("10/01/2019 00:00", test.getStartOfTheMonth().toString());
     }
 
     @Test
-    public void getEndOfTheMonthFromDateTimeStringShouldReturnCorrectDateTimeString() {
+    public void getEndOfTheMonth_DateTimeString_expectedDateTimeString() {
         String date = "10/16/2019";
         DateTime test = new DateTime(date + " 23:59");
         assertEquals("10/31/2019 23:59", test.getEndOfTheMonth().toString());
