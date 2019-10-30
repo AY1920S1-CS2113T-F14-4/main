128512d247be5e8ddbeffcc12a23eb80f5e9941f
ttippinyu <StartNow25>
2019-10-31 02:32:34 +0800

    made command abstract

--- src/main/java/spinbox/commands/Command.java
+++ src/main/java/spinbox/commands/Command.java
@@ -13,41 +13,16 @@ public abstract class Command {
     private static final String NOT_ON_MODULE_PAGE = "Please be on a module page or indicate "
             + "the specific module this action is for.";
     private boolean isExit;
-    private boolean isFileCommand;
 
-    // New execute needs to be made abstract
-    public String execute(ModuleContainer moduleContainer, ArrayDeque<String> pageTrace, Ui ui, boolean guiMode)
-            throws SpinBoxException {
-        return "Blank";
-    }
-
-    // Old execute
-    public String execute(TaskList taskList, Storage storage, Ui ui, boolean guiMode) throws SpinBoxException {
-        return null;
-    }
+    public abstract String execute(ModuleContainer moduleContainer, ArrayDeque<String> pageTrace, Ui ui, boolean guiMode)
+            throws SpinBoxException;
 
     public boolean isExit() {
         return isExit;
     }
 
-    /**
-     * Check whether it is file command.
-     * @return whether it is file command.
-     */
-    public boolean isFileCommand() {
-        return isFileCommand;
-    }
-
     public void setExit(boolean exit) {
         isExit = exit;
-    }
-
-    /**
-     * Set the isFileCommand value.
-     * @param fileCommand the value of isFileCommand.
-     */
-    public void setFileCommand(boolean fileCommand) {
-        isFileCommand = fileCommand;
     }
 
     /**
